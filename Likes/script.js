8db8a40299794909a2a356180e854f701537aee8
dgaoiran <100329962+dgaoiran@users.noreply.github.com>
2022-05-18 08:49:33 -0700

    uploaded assignments

---
+++ Likes/script.js
@@ -0,0 +1,30 @@
+function like(l1){
+    console.log("plusone")
+    document.getElementById(l1).innerHTML++;
+}
+
+function like(l2){
+    console.log("plusone")
+    document.getElementById(l2).innerHTML++;
+}
+
+function like(l3){
+    console.log("plusone")
+    document.getElementById(l3).innerHTML++;
+}
+
+// var likes = [9, 12, 9];
+// // these are the numbers we want to start those likes at
+
+// var spans = [
+//     document.querySelector("#post-1")
+// ];
+// // this is how we are selecting the inner text. You'll need to do this for each span.
+
+// function like(id) {
+//     likes[id]++;
+// //here we are grabbing the id like(1) and incrementing it
+//     spans[id].innerHTML = likes[id];
+// // here we are just displaying the number in the inner text with the string "like(s)" at the end.
+// }
+// //this is one function to handle all of the calls.
